d14b6b31ea13eefbcc676736feca411451bdc6ef
chao-hui.huang <chao-hui.huang@pfizer.com>
2026-03-17 00:38:10 -0700

    renamed:    install.sh -> build.sh 	deleted:    installation.txt

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
-sudo systemctl  daemon-reload; 
+sudo systemctl daemon-reload; 
 sudo systemctl restart ollama; 
 openclaw daemon stop; 
-npm install; 
+npm install @sinclair/typebox
+npm install typescript
+npm install --save-dev @types/node
+npm install;  
 npm run build; 
 openclaw plugins uninstall clawpyter; 
 openclaw plugins install -l `pwd`; 
